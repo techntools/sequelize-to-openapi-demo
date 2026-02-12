29591fe5343342ef7173cbb82f5d08cf94dcfe37
Santosh Bandichode <santoshg550@gmail.com>
2026-02-12 20:19:00 +0530

    Include RANGE, GEOGRAPHY, HSTORE columns

--- src/company/controller.ts
+++ src/company/controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request } from 'express'
 
 import { plainToClass } from 'class-transformer'
 
--- src/company/models/company.ts
+++ src/company/models/company.ts
@@ -1,4 +1,4 @@
-import { NotEmpty, Column, Table, Model } from 'sequelize-typescript'
+import { DataType, NotEmpty, Column, Table, Model } from 'sequelize-typescript'
 
 
 @Table
@@ -6,6 +6,26 @@ export default class Company extends Model {
     @NotEmpty
     @Column
     name: string
+
+    @Column({
+        type: DataType.RANGE(DataType.INTEGER)
+    })
+    capacity: string
+
+    @Column({
+        type: DataType.GEOGRAPHY
+    })
+    headquarterLocation: string
+
+    @Column({
+        type: DataType.HSTORE
+    })
+    management: {
+        founder: string,
+        cto: string,
+        ceo: string,
+        coo: string,
+    }
 }
 
 
